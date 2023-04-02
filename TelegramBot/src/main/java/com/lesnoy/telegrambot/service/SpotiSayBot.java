cbb660ed0c80883c3a040b44844c68950ae91d1f
Lesn0y <kilriljob@gmail.com>
2023-04-02 22:31:42 +0300

    Alpha version Bot

--- TelegramBot/src/main/java/com/lesnoy/telegrambot/service/SpotiSayBot.java
+++ TelegramBot/src/main/java/com/lesnoy/telegrambot/service/SpotiSayBot.java
@@ -28,33 +28,50 @@ public class SpotiSayBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
 
             long chatId = update.getMessage().getChatId();
-
+            String username = update.getMessage().getFrom().getUserName();
+            System.out.println("FROM - " + username);
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(chatId);
 
-            try {
-                ObjectMapper mapper = new ObjectMapper();
+            if (update.getMessage().getText().equals("auth")) {
+                try {
+                    OkHttpClient client = new OkHttpClient();
 
-                OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://localhost:8080/auth/registration?username=" + username)
+                            .build();
 
-                Request request = new Request.Builder()
-                        .url("http://localhost:8080/api/v1/track")
-                        .build();
+                    Response response = client.newCall(request).execute();
 
-                Response response = client.newCall(request).execute();
+                    String responseMessage = response.body().string();
+                    System.out.println(responseMessage);
+                    sendMessage.setText(responseMessage);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (update.getMessage().getText().equals("track")) {
+                try {
+                    OkHttpClient client = new OkHttpClient();
 
-                String responseMessage = response.body().string();
+                    Request request = new Request.Builder()
+                            .url("http://localhost:8080/api/v1/track?username=" + username)
+                            .build();
 
-                sendMessage.setText(responseMessage);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                    Response response = client.newCall(request).execute();
+
+                    String responseMessage = response.body().string();
+
+                    sendMessage.setText(responseMessage);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
-
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {
                 throw new RuntimeException(e);
             }
+
         }
     }
 
